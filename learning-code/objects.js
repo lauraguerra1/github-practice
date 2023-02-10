4a363c4e61b862663df7c9478671c583ef5a1b4b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-10 12:03:12 -0500

    Create a zoo

--- learning-code/objects.js
+++ learning-code/objects.js
@@ -5,5 +5,22 @@ var tweet1 = {
     date: "Jan 25, 2022",
     likes: 21,
     tweetContent: "My M1 Macbook Pro arrived 🤓"
-}
+};
 console.log(tweet1.date);
+console.log(Object.keys(tweet1));
+console.log(Object.values(tweet1));
+
+console.log("=============================================");
+
+var zoo = {
+    giraffes: 3,
+    zebras: 12,
+    hippos: 2
+};
+console.log(Object.keys(zoo));
+console.log(Object.values(zoo));
+console.log(zoo.giraffes);
+zoo.monkeys = 5
+console.log(zoo.monkeys);
+zoo.whales = 7
+console.log(Object.keys(zoo));
