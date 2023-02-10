ba5fffb8c4da1f2d3bd778da495f593d9987b036
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-10 11:29:34 -0500

    Make an object

---
+++ learning-code/objects.js
@@ -0,0 +1,9 @@
+var tweet1 = {
+    userHandle: "@MechEngSanchez",
+    userName: "Leirá Sanchez⚡️",
+    time: "7:06 am",
+    date: "Jan 25, 2022",
+    likes: 21,
+    tweetContent: "My M1 Macbook Pro arrived 🤓"
+}
+console.log(tweet1.date);
